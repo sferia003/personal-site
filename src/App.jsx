713ece4a6c69263a462a8895cd32cbbf198a1f6d
Stephen Feria <stephen.feria003@gmail.com>
2022-08-07 23:31:38 -0700

    Removed particles

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import * as React from 'react';
 
 import { Grid, Typography } from '@mui/material';
 import { Button } from '@mui/material';
-import { Box } from '@mui/system';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@emotion/react';
 
-import { loadLinksPreset } from "tsparticles-preset-links";
-import Particles from 'react-tsparticles';
-
 import { isMobile } from 'react-device-detect';
+
 import Icons from './Icons';
 
 const theme = createTheme({
@@ -33,29 +30,9 @@ const theme = createTheme({
 });
 
 const App = () => {
-  const particlesInit = async (props) => {
-    await loadLinksPreset(props)
-  };
 
   return (
     <ThemeProvider theme={theme}>
-      <Box>
-        <Particles init={particlesInit} options={{
-          preset: 'links',
-          background: {
-            color: '#ffffff'
-          },
-          particles: {
-            color: '#000000',
-            links: {
-              color: '#191926'
-            },
-            number: {
-              value: (isMobile) ? 25 : 100
-            }
-          },
-        }} />
-      </Box>
       <Button />
       <Grid container justifyContent={'center'} alignItems={'center'} position='absolute' minHeight={'100vh'}>
         <Grid item>
