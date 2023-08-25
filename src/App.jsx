991bd9787ea748ef187c16053756a0e7c3caea83
Stephen Feria <stephen.feria003@gmail.com>
2023-08-25 11:14:17 -0400

    Made a bit more responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { styled } from '@mui/system';
 import { Typography, Divider, Link, Stack} from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
@@ -20,19 +21,32 @@ const TextOverlay = styled('div')({
   color: 'white',
 });
 
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 700,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
 const name = "Stephen Feria"
 const description = "I study Computer Science at Purdue. I am interested in functional programming, cloud computing, and distributed systems. "
  + "Feel free to contact me below."
 const App = () => {
   return (
+      <ThemeProvider theme={theme}>
       <ImageContainer>
        <TextOverlay>
-        <Card raised variant='outlined' sx={{minWidth: '15em', maxWidth: '45em', backgroundColor: '#FFFFFF'}}>
+        <Card variant='outlined' sx={{minWidth: '15em', maxWidth: '45em', backgroundColor: '#FFFFFF'}}>
             <CardContent>
-                <Typography sx={{paddingBottom: '1em', color: '#313335'}} variant="h4">{name}</Typography>
-                <Typography sx={{paddingBottom: '0.5em'}}variant="body1">{description}</Typography>
+                <Typography sx={{paddingBottom: '1em', color: '#313335', fontSize: {xs: '1.5em', sm: '2em'}}} variant="h4">{name}</Typography>
+                <Typography sx={{paddingBottom: '0.5em', fontSize: {xs: '0.5em', sm: '1em'}}}variant="body2">{description}</Typography>
                 <Divider sx={{marginBottom: '0.5em'}} />
-                <Stack sx={{justifyContent: 'center'}} direction='row' spacing={2}>
+                <Stack sx={{justifyContent: 'center', fontSize: {xs: '0.5em', sm: '1em'}}} direction='row' spacing={2}>
                     <Link underline='hover' color='#313335' href="mailto:sferia003@gmail.com">email</Link>
                     <Link underline='hover' color='#313335' href="https://github.com/sferia003">github</Link>
                     <Link underline='hover' color='#313335' href="https://www.linkedin.com/in/stephen-feria-10b99b19a">linkedin</Link>
@@ -43,6 +57,7 @@ const App = () => {
         </Card>
        </TextOverlay>
       </ImageContainer>
+      </ThemeProvider>
   );
 };
 
